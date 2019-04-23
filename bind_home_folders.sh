f63cef52d414639c9105e1714a91ced916b9383c
Kevin <mostert.k@gmail.com>
2019-04-23 03:40:31 +0200

    Added Games Folder

--- bind_home_folders.sh
+++ bind_home_folders.sh
@@ -4,9 +4,11 @@
 #08/04/2019
 #Place this in /usr/local/bin/ or /home/yourusername/bin
 
+sleep 15s
 mount --bind /mnt/Storage/Documents /home/kevin/Documents
 mount --bind /mnt/Storage/Downloads /home/kevin/Downloads
 mount --bind /mnt/Storage/Music /home/kevin/Music
 mount --bind /mnt/Storage/Pictures /home/kevin/Pictures
 mount --bind /mnt/Storage/Videos /home/kevin/Videos
 mount --bind /mnt/Storage/Dropbox /home/kevin/Dropbox
+mount --bind /mnt/Storage/Games /home/kevin/Games
